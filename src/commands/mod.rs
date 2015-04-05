325792e66f9026c4ea450e78dd0709eb602cb1af
Peter Horne <peterhorne92@gmail.com>
2015-04-05 17:14:44 +0100

    WIP: Access over TCP

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,4 +1,6 @@
 use database::Database;
+use std::io::{BufRead, BufStream, Read, Write};
+use std::io;
 
 mod stock;
 use self::stock::Stock;
@@ -9,7 +11,7 @@ use self::consume::Consume;
 pub fn from_str(args: &str) -> Result<Box<Command>, &'static str> {
     match args.trim().split(' ').collect::<Vec<&str>>().as_ref() {
         [name, args..] => match name {
-            "STOCK" =>   Stock::from_str(args),
+            "STOCK"   => Stock::from_str(args),
             "CONSUME" => Consume::from_str(args),
             _ => Err("Unrecognised command."),
         },
@@ -20,4 +22,30 @@ pub fn from_str(args: &str) -> Result<Box<Command>, &'static str> {
 pub trait Command {
     fn to_string(&self) -> String;
     fn execute(&self, database: &mut Database) -> Box<Command>;
+}
+
+pub struct CommandStream<T: Read + Write> {
+    inner: BufStream<T>
+}
+
+impl<T: Read + Write> CommandStream<T> {
+    pub fn new(inner: BufStream<T>) -> Self {
+        CommandStream { inner: inner }
+    }
+
+    pub fn write(&mut self, command: String) -> io::Result<usize> {
+        self.inner.write(command.as_bytes())
+    }
+
+    pub fn flush(&mut self) -> io::Result<()> {
+        self.inner.flush()
+    }
+
+    pub fn next(&mut self) -> Option<Result<Box<Command>, &'static str>> {
+        let mut buffer = String::new();
+        match self.inner.read_line(&mut buffer) {
+            Ok(0) | Err(_) => None,
+            Ok(length) => Some(from_str(&buffer)),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![feature(convert)]
 #![feature(slice_patterns)]
 
+use std::io::{BufStream, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 mod database;
 mod journal;
 mod input;
@@ -9,7 +14,7 @@ mod commands;
 use database::Database;
 use journal::Journal;
 // use input::Input;
-use commands::Command;
+use commands::{Command, CommandStream};
 
 fn main() {
     let mut database = Database::new();
@@ -21,29 +26,46 @@ fn main() {
             .execute(&mut database);
     }
 
-    println!("Database: {:?}", database);
+    let journal_mutex = Arc::new(Mutex::new(journal));
+    let db_mutex = Arc::new(Mutex::new(database));
 
-    // for line in Input::new() {
-    //     match line {
-    //         Err(why)    => println!("{}", why),
-    //         Ok(command) => {
-    //             journal.write(&command);
-    //             let result = command.process(&resources, &consumers);
-    //             println!("{:?}", result);
-    //         },
-    //     }
-    // }
+    let socket = "127.0.0.1:8000";
+    let listener = TcpListener::bind(socket).unwrap();
+    println!("Listing on {}", socket);
 
-    // let reservations = get_reservations();
+    for connection in listener.incoming() {
+        match connection {
+            Ok(mut connection) => {
+                let connection = CommandStream::new(BufStream::new(connection));
+                let journal = journal_mutex.clone();
+                let database = db_mutex.clone();
+                thread::spawn(move|| {
+                    handle_connection(connection, database, journal);
+                });
+            }
+            Err(e) => { println!("Error: {:?}", e); }
+        }
+    }
+}
 
-    // let mut input = HashMap::new();
-    // input.insert("aaaa", 2);
-    // input.insert("cccc", 1);
-
-    // let existing = match reservations.get("1234") {
-    //     Some(v) => diff(v, &input),
-    //     None => input,
-    // };
-
-    // println!("{:?}", existing);
+fn handle_connection<T: Read + Write>(mut connection: CommandStream<T>, db_mutex: Arc<Mutex<Database>>, journal_mutex: Arc<Mutex<Journal>>) {
+    loop {
+        match connection.next() {
+            Some(Ok(command)) => {
+                let mut database = db_mutex.lock().unwrap();
+                let mut journal = journal_mutex.lock().unwrap();
+                let result = command.execute(&mut *database);
+                // journal.write(&result);
+                connection.write(result.to_string());
+                connection.write('\n'.to_string());
+                connection.flush();
+            }
+            Some(Err(why)) => {
+                connection.write(why.to_string());
+                connection.write('\n'.to_string());
+                connection.flush();
+            }
+            _ => { break; }
+        }
+    }
 }
